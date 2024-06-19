107db61bcd0ce38b1712bf2c9c9dd69f5acb27c9
Franco Paraván <francoparavan@gmail.com>
2024-06-19 18:01:13 +0000

    guardado

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,34 @@
-//import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
-//import your own components
-import Home from "./component/home.jsx";
+const SecondsCounter = ({ digitFour, digitThree, digitTwo, digitOne }) => {
+return (
+<div className="bigCounter">
+<div className="calendar">
+<i className="bi bi-clock"></i>
+</div>
+<div className="four">{digitFour % 10}</div>
+<div className="three">{digitThree % 10}</div>
+<div className="two">{digitTwo % 10}</div>
+<div className="one">{digitOne % 10}</div>
+</div>
+);
+};
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let counter = 0
+setInterval(() => {
+const four = Math.floor(counter / 1000);
+const three = Math.floor(counter / 100);
+const two = Math.floor(counter / 10);
+const one = Math.floor(counter / 1);
+console.log(four, three, two, one);
+counter++;
+const root = createRoot(document.getElementById("app"));
+root.render(<SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />);
+}, 1000);
 
